b28798c56e886d1b6bdc69cac38ee4e10d7added
Mdoodl <127056332+Mdoodl@users.noreply.github.com>
2023-03-29 06:29:26 +0300

    Add files via upload

---
+++ members/member.identity.ts
@@ -0,0 +1,13 @@
+export class Member {
+    id: number;
+    fullname: string;
+    quote: string;
+    red_data: string;
+    bir_data: string;
+    role: string;
+    city: string;
+    about: string;
+    password: string;
+    email: string;
+  }
+  
