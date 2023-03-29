1c477607bf50f3c04fa037de2658ccdcaa512b45
Mdoodl <127056332+Mdoodl@users.noreply.github.com>
2023-03-29 06:22:53 +0300

    Add files via upload

---
+++ comments/comments.identity.ts
@@ -0,0 +1,7 @@
+export class Comment {
+    id: number;
+    senderID: number;
+    forumname: string;
+    text: string;
+  }
+  
---
+++ comments/comments.service.ts
@@ -0,0 +1,40 @@
+import { HttpStatus, Injectable } from "@nestjs/common";
+import { DatasourceService } from "src/datasource/datasource.service";
+import { Comment } from "./comments.identity";
+
+@Injectable()
+export class CommentsService {
+    constructor(private readonly datasourceService: DatasourceService) {}
+    create(comment: Comment) {
+        this.datasourceService.getComments().push(comment);
+        return comment;
+      }
+      findOne(id: number) {
+        return this.datasourceService
+          .getComments()
+          .find((Comment) => Comment.id === id);
+      }
+
+      findAll(): Comment[] {
+        return this.datasourceService.getComments();
+      }
+      update(id: number, updatedComment: Comment) {
+        const index = this.datasourceService
+          .getComments()
+          .findIndex((Comment) => Comment.id === id);
+        this.datasourceService.getComments()[index] = updatedComment;
+        return this.datasourceService.getComments()[index];
+      }
+
+      remove(id: number) {
+        const index = this.datasourceService
+          .getComments()
+          .findIndex((comment) => comment.id === id);
+        this.datasourceService.getComments().splice(index, 1);
+        return HttpStatus.OK;
+      }
+    
+    
+    
+    
+}
