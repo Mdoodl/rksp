2b16790e12963c422e00351cc1c69000c07d1d0e
Mdoodl <127056332+Mdoodl@users.noreply.github.com>
2023-04-26 01:46:17 +0300

    Add files via upload

---
+++ controllers/comments.controller.ts
@@ -0,0 +1,39 @@
+import { CommentsService } from "../servicies/comments.service";
+import { Body, Controller, Delete, Get, Param, Post, Put} from '@nestjs/common';
+import { Comment } from "../identities/comments.identity";
+
+
+@Controller('comments')
+export class CommentsController {
+  constructor(private readonly commentsService: CommentsService) {}
+
+@Get()
+  findAll() {
+    return this.commentsService.findAll();
+  }
+
+@Get(':id')
+  findOne(@Param('id') id: string) {
+    return this.commentsService.findOne(+id);
+  }
+ 
+@Put(':id')
+  update(@Param('id') id: string, @Body() updateComment: Comment) {
+    return this.commentsService.update(+id, updateComment);
+  }
+
+@Post()
+  create(@Body() createComment: Comment) {
+    return this.commentsService.create(createComment);
+  }
+
+  @Delete(':id')
+  remove(@Param('id') id: string) {
+    return this.commentsService.remove(+id);
+  }
+
+
+
+}
+
+
---
+++ controllers/members.controller.ts
@@ -0,0 +1,39 @@
+import { MembersService } from "../servicies/members.service";
+import { Body, Controller, Delete, Get, Param, Post, Put} from '@nestjs/common';
+import { Member } from "../identities/member.identity";
+
+
+@Controller('members')
+export class MembersController {
+  constructor(private readonly membersService: MembersService) {}
+
+@Get()
+  findAll() {
+    return this.membersService.findAll();
+  }
+
+@Get(':id')
+  findOne(@Param('id') id: string) {
+    return this.membersService.findOne(+id);
+  }
+ 
+@Put(':id')
+  update(@Param('id') id: string, @Body() updateMember: Member) {
+    return this.membersService.update(+id, updateMember);
+  }
+
+@Post()
+  create(@Body() createMember: Member) {
+    return this.membersService.create(createMember);
+  }
+
+  @Delete(':id')
+  remove(@Param('id') id: string) {
+    return this.membersService.remove(+id);
+  }
+
+
+
+}
+
+
